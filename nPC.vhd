e60e40d7dc85a95d381c5cd4444b70292c573804
shoyos420 <shoyos420@hotmail.com>
2016-10-13 06:49:21 -0500

    otros 3  modulos

---
+++ nPC.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity nPC is
+    Port ( address : in  STD_LOGIC_VECTOR (31 downto 0);
+			  reset : in  STD_LOGIC;
+           clkFPGA : in  STD_LOGIC;
+           nextInstruction : out  STD_LOGIC_VECTOR (31 downto 0));
+end nPC;
+
+architecture arqNPC of nPC is
+
+begin
+process(clkFPGA, address, reset)
+	begin
+		if(reset = '1')then
+			nextInstruction <= (others=>'0');
+		else
+			if(rising_edge(clkFPGA))then
+				nextInstruction <= address;
+			end if;
+		end if;
+	end process;
+end arqNPC;
