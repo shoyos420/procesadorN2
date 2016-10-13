1d6795d4d1f3c3eedaaa311ecb983aa3103bbd21
shoyos420 <shoyos420@hotmail.com>
2016-10-13 06:51:34 -0500

    IM corregido

---
+++ Instruction_Memory.vhd
@@ -0,0 +1,44 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use std.textio.all;
+
+entity instruction_Memory is
+    Port ( 
+			  address : in  STD_LOGIC_VECTOR (31 downto 0);
+           reset : in  STD_LOGIC;
+           outInstruction : out  STD_LOGIC_VECTOR (31 downto 0));
+end instruction_Memory;
+
+architecture arqInstruction_Memory of instruction_Memory is
+
+	type rom_type is array (0 to 63) of std_logic_vector (31 downto 0);
+		
+	impure function InitRomFromFile (RomFileName : in string) return rom_type is
+		FILE RomFile : text open read_mode is RomFileName;
+		variable RomFileLine : line;
+		variable temp_bv : bit_vector(31 downto 0);
+		variable temp_mem : rom_type;
+		begin
+			for I in rom_type'range loop
+				readline (RomFile, RomFileLine);
+				read(RomFileLine, temp_bv);
+				temp_mem(i) := to_stdlogicvector(temp_bv);
+			end loop;
+		return temp_mem;
+	end function;
+	
+	signal instructions : rom_type := InitRomFromFile("instrucciones.txt");
+	
+begin
+	process(reset,address, instructions)--clk
+	begin
+		
+			if(reset = '1')then
+				outInstruction <= (others=>'0');
+			else
+				outInstruction <= instructions(conv_integer(address(5 downto 0)));
+			end if;
+		
+	end process;
+end arqInstruction_Memory;
